87b2976d0ebd3613fecad4adf80214d48c42e5df
valterlan <valterlan@Valterlan>
2016-09-08 19:59:32 -0400

    login

---
+++ cabecalho.php
@@ -0,0 +1,16 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.css" rel="stylesheet" />
+        <link href="css/estudo.css" rel="stylesheet" />
+        <title></title>
+    </head>
+    <body>
+                
+            
+            
+            
+            
+            
+ 
---
+++ login.php
@@ -0,0 +1,21 @@
+<?php include './cabecalho.php' ;?>
+<form>
+    
+    <div class="container">
+     <table   >
+        <tr>
+            <td>Login:</td> 
+            <td><input class="form-control" type="text" name="nome" /><br/></td>
+        </tr>
+        <tr>
+            <td>Senha:</td> 
+            <td><input class="form-control" type="text" name="senha" /><br/></td>
+        </tr>
+        <tr>
+       <td> <input class="btn btn-primary" type="submit" value="Cadastrar" /></td>
+        </tr>
+       </table>
+    </div>
+</form>
+
+<?php include './rodape.php'?>
