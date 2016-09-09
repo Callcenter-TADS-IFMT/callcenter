6569b17fffd5a7ff5e5e685ae1687d5d564d3f9c
medei <medei@10.1.2.156>
2016-09-08 21:24:36 -0400

    bbbidbcbccwe

---
+++ chamados_pedentes.php
@@ -0,0 +1,42 @@
+    
+   <?php include_once('cabecalho.php'); ?>
+
+<div class="row">
+    <div class="col-md-3 col-xs-12 col-lg-3">
+        <?php include_once('sidebar.php'); ?>
+    </div>
+                <h2>Chamados Pedentes</h2>
+    <div class="container">
+        <div class="principal">
+            <form>
+              <table class="table" >
+                    </br>
+                    <tr>
+                        <td>Nome do Cliente</td>
+
+                    </tr>
+                    <tr>
+                        <td>Idade</td>
+                        <td></td>
+
+                    </tr>
+                    <tr>
+                        <td>Nome do Tecnico</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        
+                    </tr>
+                    <tr>
+                        
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+    <div class="col-md-9 col-xs-12 col-lg-3">
+    </div>
+</div>
+
+<?php include_once('rodape.php'); ?>
