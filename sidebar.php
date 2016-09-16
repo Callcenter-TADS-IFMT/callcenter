be13c16a2c00c79e3050305fb7613ce3b3a7efb3
Aldair Natan <aldair.ngomes@gmail.com>
2016-09-15 20:10:42 -0400

    Adicionando itens do Menu

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,8 @@
 
 <div class="list-group">
-  <a href="#" class="list-group-item ">menu</a>
-  <a href="#" class="list-group-item ">menu</a>
-  <a href="#" class="list-group-item ">menu</a>
-  <a href="#" class="list-group-item ">menu</a>
-  <a href="#" class="list-group-item ">menu</a>
-  <a href="#" class="list-group-item ">menu</a>
-  
+  <a href="./chamados_pedentes.php" class="list-group-item ">Chamados pendentes</a>
+  <a href="#" class="list-group-item ">Chamados em atendimento</a>
+  <a href="./chamados_finalizados.php" class="list-group-item ">Chamados Finalizados</a>
+  <a href="./chamado.php" class="list-group-item ">Cadastro de Chmados</a>
+  <a href="#" class="list-group-item ">Cadastro de Funcionários</a>
 </div>
