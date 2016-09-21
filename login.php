a42f220184f304da3c099a35058537e56b132e8e
TADS <TADS@Lab02-est25>
2016-09-20 22:10:09 -0400

    Alterações no Login

--- login.php
+++ login.php
@@ -1,21 +1,30 @@
-<?php include './cabecalho.php' ;?>
+<?php include './cabecalho.php'; ?>
 <form>
-    
+
     <div class="container">
-     <table   >
-        <tr>
-            <td>Login:</td> 
-            <td><input class="form-control" type="text" name="nome" /><br/></td>
-        </tr>
-        <tr>
-            <td>Senha:</td> 
-            <td><input class="form-control" type="text" name="senha" /><br/></td>
-        </tr>
-        <tr>
-       <td> <input class="btn btn-primary" type="submit" value="Cadastrar" /></td>
-        </tr>
-       </table>
+
+        <div class="col-md-12" style="margin-left:500px">
+
+
+            <br/><br/><br/><br/><br/><br/><br/>
+
+            <table>
+                <tr>
+                    <td>   <img src="images/women.png" style="width: 300px"></td>
+                </tr>
+                <tr>
+                    <td><input style="align-items: center" type="email" class="form-control" id="exampleInputEmail3" placeholder="Login"/><br/></td>
+                </tr>
+                <tr>
+                    <td><input type="password" class="form-control" id="login" placeholder="Senha"/><br/></td>
+                </tr>
+                <tr>
+                    <td> <input class="btn btn-primary" type="submit" value="Login" style="width: 300px" /></td>
+                </tr>
+
+            </table>
+
+        </div>
     </div>
 </form>
-
-<?php include './rodape.php'?>
+<?php include './rodape.php' ?>
